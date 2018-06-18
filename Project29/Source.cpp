d8bad111611ddc049c061195533d73df19758fce
КимС <@224_5>
2018-06-18 21:50:26 +0600

    Bird

--- Project29/Source.cpp
+++ Project29/Source.cpp
@@ -2,8 +2,15 @@
 #include <SFML\System\Time.hpp>
 #include "Player.h"
 #include <iostream>
+#include "map.h"
 
 int main() {
+	sf::Image map_image;//объект изображения для карты
+	map_image.loadFromFile("images/map.png");//загружаем файл для карты
+	sf::Texture map;//текстура карты
+	map.loadFromImage(map_image);//заряжаем текстуру картинкой
+	sf::Sprite s_map;//создаём спрайт для карты
+	s_map.setTexture(map);//заливаем текстуру спрайтом
 	float CurrentFrame = 0;//хранит текущий кадр
 	sf::RenderWindow window(sf::VideoMode(1200, 600), "SFML works!");
 	sf::Clock clock;
@@ -52,8 +59,20 @@ int main() {
 
 			}
 		}
-		p.sprite.move(0, 0.01);
+		p.sprite.move(0, 0.05);
 		window.clear(sf::Color::Black);
+		for (int i = 0; i < HEIGHT_MAP; i++)
+			for (int j = 0; j < WIDTH_MAP; j++)
+			{
+				if (TileMap[i][j] == ' ')  s_map.setTextureRect(sf::IntRect(0, 0, 32, 32)); //если встретили символ пробел, то рисуем 1й квадратик
+				if (TileMap[i][j] == 's')  s_map.setTextureRect(sf::IntRect(32, 0, 32, 32));//если встретили символ s, то рисуем 2й квадратик
+				if ((TileMap[i][j] == '0')) s_map.setTextureRect(sf::IntRect(64, 0, 32, 32));//если встретили символ 0, то рисуем 3й квадратик
+
+
+				s_map.setPosition(j * 32, i * 32);//по сути раскидывает квадратики, превращая в карту. то есть задает каждому из них позицию. если убрать, то вся карта нарисуется в одном квадрате 32*32 и мы увидим один квадрат
+
+				window.draw(s_map);//рисуем квадратики на экран
+			}
 		//p.update(time);
 		window.draw(p.sprite);
 		window.display();
